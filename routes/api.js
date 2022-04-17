247ce4bbee9c5e97aadfc5e461d4f466c6e4b1b8
rxavio <rucahoxavi@gmail.com>
2022-04-17 13:52:10 +0200

    fttch user info

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const https = require('https');
+const dotenv = require('dotenv');
+dotenv.config();
+
+router.get('/github/userinfo/:user', async function (req, res) {
+    const user = req.params.user;
+    const options = {
+        hostname: 'api.github.com',
+        path: '/users/' + user,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
+        },
+        OAUth:process.env.TOKEN
+     
+    }
+    https.get(options, function (apiResponse) {
+        apiResponse.pipe(res);
+    }).on('error', (e) => {
+        console.log(e);
+        res.status(500).send('Something wnent wrong!');
+    })
+})
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-
+const api = require('./routes/api');
 
 const cors = require('cors');
 
@@ -17,11 +19,9 @@ app.use(function (req, res, next) {
     next();
 });
 
-
+app.use('/api', api);
 app.get('/', function (req, res) {
     res.send('Server is up and running!');
 })
-
-app.listen(3000, function () {
-    console.log('Server is listening on 3000');
-})
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, console.log(`server run on port ${PORT}`));
